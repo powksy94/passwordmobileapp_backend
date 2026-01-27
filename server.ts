f4160d654057b02728c582466eb6f586264a3a9d
Uzan <Uzan@exemple.com>
2026-01-27 13:25:08 +0100

    revise middleware

--- server.ts
+++ server.ts
@@ -1,17 +1,18 @@
 import app from "./app.js";
-import { connectMongo } from "./src/config/mongo.ts";
-import { pool } from "./src/config/postgres.ts";
-import { PORT } from "./src/config/env.ts";
+import { connectMongo } from "./src/config/mongo";
+import { pool } from "./src/config/postgres";
+import { PORT } from "./src/config/env";
 
 (async () => {
   try {
     await connectMongo();
     await pool.connect();
+
     console.log("✅ PostgreSQL connected");
 
     app.listen(PORT || 5000, () => console.log(`Server running on port ${PORT || 5000}`));
   } catch (err) {
-    console.error(err);
+    console.error("Server failed",err);
     process.exit(1);
   }
 })();
