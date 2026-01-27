53d5c744c7e75624dea56757594efca3f75d64c9
Uzan <Uzan@exemple.com>
2026-01-27 16:29:06 +0100

    revise middleware

--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
 import express from "express";
-import { authMiddleware} from "./src/middleware/auth.middleware";
 import { errorMiddleware } from "./src/middleware/error.middleware";
+import authRoutes from "./src/routes/authRoutes";
+import vaultRoutes from "./src/routes/vaultRoutes";
 
 const app = express();
+
 app.use(express.json());
-
-
 
 app.use("/auth", authRoutes);
 app.use("/vault", vaultRoutes);
+
+// middleware d'erreurs EN DERNIER
+app.use(errorMiddleware);
 
 export default app;
