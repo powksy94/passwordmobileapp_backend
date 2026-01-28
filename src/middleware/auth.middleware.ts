280129f90e27ffc554a84d620cf54c492bd9a5a3
Uzan <Uzan@exemple.com>
2026-01-28 09:47:37 +0100

    revise middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -4,20 +4,33 @@ import { JWT_SECRET } from "../config/env";
 
 interface JwtPayload {
   id: string;
+  role: "admin" | "user";
   iat: number;
   exp: number;
 }
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader) return res.status(401).json({ error: "Authorization header missing" });
+  if (!authHeader) 
+    return res.status(401).json({ error: "Authorization header missing" });
 
   const token = authHeader.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "Token missing" });
+  if (!token) 
+    return res.status(401).json({ error: "Token missing" });
 
   try {
     const payload = jwt.verify(token, JWT_SECRET!) as JwtPayload;
-    req.user = { id: payload.id };
+
+    req.user = 
+    { 
+      id: payload.id,
+      role: payload.role,
+     };
+     
     next();
   } catch (err) {
     return res.status(401).json({ error: "Invalid or expired token" });
